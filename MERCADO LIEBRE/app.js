44d82c38fdcdd8682e3eb12d7e04bbdd8775c31e
faacku010 <fachundo20@gmail.com>
2023-08-07 10:51:02 -0300

    primer commit agregando todo el proyecto terminado

---
+++ MERCADO LIEBRE/app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+const port = process.PORT || 3001;
+app.listen(port, () => console.log('Servidor corriendo en el puerto ${port}'))
+
+/* const PORT = 3050; */
+
+ app.use(express.static(path.resolve(__dirname, 'public')))
+
+/* app.listen(PORT, () => console.log('Servidor corriendo 3050')); */
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './src/views/home.html'))
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './src/views/login.html'))
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './src/views/register.html'))
+})
+
+
+
+/* const express = require('express');
+const app = express();
+const path = require("path");
+
+app.listen (3030, () =>
+    console.log("Corriendo en servidor 3030")
+);
+
+app.use(express.static(path.resolve('__dirname, public')));
+
+app.get ("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./src/views/home.html"));
+});
+
+app.get ("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./src/views/login.html"));
+});
+
+app.get ("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./src/views/register.html"));
+}); */
