279eaffff8e5fb560e00b35a09479822fb153025
faacku010 <fachundo20@gmail.com>
2024-03-20 12:29:44 -0300

    maqueta realizada desde 0

--- app.js
+++ app.js
@@ -1,49 +1,25 @@
 const express = require('express');
-const app = express();
-
 const path = require('path');
 
-const port = process.env.PORT || 3040;
-app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
+const app = express();
 
-/* const PORT = 3050; */
+app.use( express.static(path.resolve(__dirname, './public')) );
 
- app.use(express.static(path.resolve(__dirname, 'public')));
+/* const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath) ); */
 
-/* app.listen(PORT, () => console.log('Servidor corriendo 3050')); */
+app.listen(3020,() => {
+    console.log(`'servidor corriendo en puerto http://localhost:3020/`);
+});
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './src/views/home.html'))
-});
+    res.sendFile(path.resolve(__dirname, './src/views/home.html'));
+})
 
 app.get('/login', (req, res) => {
     res.sendFile(path.resolve(__dirname, './src/views/login.html'))
-});
+})
 
 app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname, './src/views/register.html'))
-});
-
-
-
-/* const express = require('express');
-const app = express();
-const path = require("path");
-
-app.listen (3030, () =>
-    console.log("Corriendo en servidor 3030")
-);
-
-app.use(express.static(path.resolve('__dirname, public')));
-
-app.get ("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./src/views/home.html"));
-});
-
-app.get ("/login", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./src/views/login.html"));
-});
-
-app.get ("/register", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./src/views/register.html"));
-}); */
+})
